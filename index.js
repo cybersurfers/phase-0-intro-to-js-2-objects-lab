88f819294a45d8588cd4da21151734ff711e0871
Anna <34191811+404unf@users.noreply.github.com>
2023-05-26 22:53:10 -0700

    Object methods lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const employee = {'name':'Dave Hamburger','streetAddress': '123 Cheese St.'}
+
+function updateEmployeeWithKeyAndValue(employee, eKey, eValue) {
+    const newEmployee = {...employee}
+    newEmployee[eKey] = eValue
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, eKey, eValue) {
+    employee[eKey] = eValue
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, eKey) {
+    const newEmployee = {...employee}
+    delete newEmployee[eKey]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, eKey) {
+    delete employee[eKey]
+    return employee
+}
